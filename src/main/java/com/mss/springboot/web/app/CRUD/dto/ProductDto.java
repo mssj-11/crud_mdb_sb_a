d57d45aff37bffbd0250354b67b3732453ff35c6
Mike <80673026+mssj-11@users.noreply.github.com>
2023-07-11 23:23:33 -0600

    DTO - ProductDto

---
+++ src/main/java/com/mss/springboot/web/app/CRUD/dto/ProductDto.java
@@ -0,0 +1,40 @@
+package com.mss.springboot.web.app.CRUD.dto;
+
+public class ProductDto {
+	
+	
+	private String name;
+	private int price;
+	
+	
+	
+	public ProductDto() {
+	}
+	
+	public ProductDto(String name, int price) {
+		super();
+		this.name = name;
+		this.price = price;
+	}
+	
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	
+	
+	
+	
+}
