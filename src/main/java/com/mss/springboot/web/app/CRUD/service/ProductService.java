78e0d0af978b2649d44b53a51b6fea8b9977d28e
Mike <80673026+mssj-11@users.noreply.github.com>
2023-07-11 23:49:21 -0600

    Service - ProductService

---
+++ src/main/java/com/mss/springboot/web/app/CRUD/service/ProductService.java
@@ -0,0 +1,65 @@
+package com.mss.springboot.web.app.CRUD.service;
+
+import java.util.Comparator;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mss.springboot.web.app.CRUD.dto.ProductDto;
+import com.mss.springboot.web.app.CRUD.entity.Product;
+import com.mss.springboot.web.app.CRUD.repository.ProductRepository;
+
+
+@Service
+public class ProductService {
+	
+	@Autowired
+	ProductRepository productRepository;
+	
+	
+	public List<Product> getAll(){
+		return productRepository.findAll();
+	}
+	
+	
+	
+	public Product getById(int id) {
+		return productRepository.findById(id).get();
+	}
+	
+	
+	public Product detele(int id) {
+		Product product = productRepository.findById(id).get();
+		productRepository.delete(product);
+		return product;
+	}
+	
+	
+	
+	//	Method ID Auto-Increment
+	private int autoIncrement() {
+		List<Product> products = productRepository.findAll();
+		return products.isEmpty() ? 1 : 
+			products.stream().max(Comparator.comparing(Product::getId)).get().getId() + 1;
+	}
+	public Product save(ProductDto dto) {
+		//	Id Auto-Increment
+		int id = autoIncrement();
+		
+		Product product = new Product(id, dto.getName(), dto.getPrice());
+		return productRepository.save(product);
+	}
+	
+	
+	
+	public Product update(int id, ProductDto dto) {
+		Product product = productRepository.findById(id).get();
+		product.setName(dto.getName());
+		product.setPrice(dto.getPrice());
+		return productRepository.save(product);
+	}
+	
+	
+	
+}
